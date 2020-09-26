05dc0acf0f1bcb406c9828b982e2145695f088da
moamenJS <moamenmedleg@hotmail.com>
2020-09-26 19:25:34 +0300

    Updates

---
+++ precache-manifest.5742124e92cf1bdc4658ecc434d6bdde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8627a5ef170e874cc12010f996259b0d",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "2fd68d1f6a97a5a701b6",
+    "url": "/react-deploy/static/css/main.66de2ebb.chunk.css"
+  },
+  {
+    "revision": "2aa89dde0859bdbbf2dc",
+    "url": "/react-deploy/static/js/2.c3b51709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.c3b51709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd68d1f6a97a5a701b6",
+    "url": "/react-deploy/static/js/main.2dd4d1bc.chunk.js"
+  },
+  {
+    "revision": "189ec86a284f5d0523d4",
+    "url": "/react-deploy/static/js/runtime-main.f39f8465.js"
+  }
+]);
